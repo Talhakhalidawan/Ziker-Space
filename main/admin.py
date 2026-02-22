fcaef996867dec5d36ca14e5cf980c49ff2d0b5f
talhakhalidawan <talhakhalidlkp@gmail.com>
2026-02-22 12:25:07 +0500

    added models to admin to show the models in the admin pannel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CustomHabit, DayLog, TodoItem
 
-# Register your models here.
+admin.site.register(CustomHabit)
+admin.site.register(DayLog)
+admin.site.register(TodoItem)
